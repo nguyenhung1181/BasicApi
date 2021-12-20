e9ad41c6cc44ba76263e404974b3f8676732c50e
nguyenhung1181 <nguyenmanhhung110801@gmail.com>
2021-12-20 18:56:34 +0700

    Update

--- BasicApi/Program.cs
+++ BasicApi/Program.cs
@@ -1,45 +1,93 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 var app = builder.Build();
 
-app.MapGet("/", () => "Xin chào Hùng");
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.Select(x => new TodoItemDTO(x)).ToListAsync());
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(new TodoItemDTO(todo))
+            : Results.NotFound());
+
+app.MapPost("/todoitems", async (TodoItemDTO todoItemDTO, TodoDb db) =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var todoItem = new Todo
+    {
+        Color = todoItemDTO.Color,
+        ToyName = todoItemDTO.ToyName,
+        Category = todoItemDTO.Category,
+        Price = todoItemDTO.Price
+    };
 
-app.UseHttpsRedirection();
+    db.Todos.Add(todoItem);
+    await db.SaveChangesAsync();
 
-var summaries = new[]
+    return Results.Created($"/todoitems/{todoItem.Id}", new TodoItemDTO(todoItem));
+});
+
+app.MapPut("/todoitems/{id}", async (int id, TodoItemDTO todoItemDTO, TodoDb db) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var todo = await db.Todos.FindAsync(id);
 
-app.MapGet("/weatherforecast", () =>
+    if (todo is null) return Results.NotFound();
+
+    todo.ToyName = todoItemDTO.ToyName;
+    todo.Color = todoItemDTO.Color;
+    todo.Category = todoItemDTO.Category;
+    todo.Price = todoItemDTO.Price;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) =>
 {
-var forecast = Enumerable.Range(1, 5).Select(index =>
-   new WeatherForecast
-   (
-       DateTime.Now.AddDays(index),
-       Random.Shared.Next(-20, 55),
-       summaries[Random.Shared.Next(summaries.Length)]
-   ))
-    .ToArray();
-return forecast;
-})
-.WithName("GetWeatherForecast");
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.Ok(new TodoItemDTO(todo));
+    }
+
+    return Results.NotFound();
+});
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+public class Todo
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public string? ToyName { get; set; }
+    public string? Color { get; set; }
+    public string? Category { get; set; }
+    public string? Price { get; set; }
+    
+}
+
+public class TodoItemDTO
+{
+    public int Id { get; set; }
+    public string? ToyName { get; set; }
+    public string? Color { get; set; }
+    public string? Category { get; set; }
+    public string? Price { get; set; }
+
+    public TodoItemDTO() { }
+    public TodoItemDTO(Todo todoItem) =>
+    (Id, ToyName, Color, Catedory, Price) = (todoItem.Id, todoItem.ToyName, todoItem.Color, todoItem.Category, todoItem.Price);
+}
+
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
 }
